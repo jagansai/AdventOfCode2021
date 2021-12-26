582ee1fa1eeb8e7aaf7a0c97f4de2ec4406ba86b
jagansai <jagan.sai@outlook.com>
2021-12-26 11:12:32 +0800

    Initial draft

---
+++ src/main/java/com/example/day1/Day01.java
@@ -0,0 +1,41 @@
+package com.example.day1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public final class Day01 {
+
+	private Day01() {
+	}
+
+	public static int solve(final List<Integer> reports) {
+
+		int prev = Integer.MIN_VALUE;
+		int count = 0;
+		for (int i = 0; i < reports.size(); i++) {
+			if (i != 0) {
+				if (prev < reports.get(i)) {
+					++count;
+				}
+				prev = reports.get(i);
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		final int count = Day01.solve(Arrays.asList(199,
+				200,
+				208,
+				210,
+				200,
+				207,
+				240,
+				269,
+				260,
+				263));
+
+		System.out.println(
+				String.format("there are %d measurements that are larger than the previous measurement", count));
+	}
+}
---
+++ src/main/java/com/example/day2/Day02.java
@@ -0,0 +1,62 @@
+
+package com.example.day2;
+
+import java.util.Arrays;
+import java.util.List;
+
+enum Direction {
+	Forward,
+	Up,
+	Down;
+
+	public static Direction getDirection(final String direction) {
+		return switch (direction.toLowerCase()) {
+			case "forward" -> Direction.Forward;
+			case "down" -> Direction.Down;
+			case "up" -> Direction.Up;
+			default -> throw new IllegalArgumentException(direction);
+		};
+	}
+
+	public static int calculateValue(Direction direction, final int value, final int total) {
+		return switch (direction) {
+			case Forward -> value + total;
+			case Up -> total - value;
+			case Down -> total + value;
+		};
+	}
+}
+
+record DirectionValue(Direction direction, int value) {
+}
+
+public class Day02 {
+
+	private static void move(final List<String> commands) {
+		int horizontalPos = 0;
+		int depth = 0;
+
+		for (var command : commands) {
+			var directionAndValue = command.split(" ");
+			final Direction direction = Direction.getDirection(directionAndValue[0]);
+			final int value = Integer.parseInt(directionAndValue[1]);
+
+			switch (direction) {
+				case Forward:
+					horizontalPos = Direction.calculateValue(Direction.Forward, value, horizontalPos);
+					break;
+				case Up:
+					depth = Direction.calculateValue(Direction.Up, value, depth);
+					break;
+				case Down:
+					depth = Direction.calculateValue(Direction.Down, value, depth);
+			}
+		}
+		System.out.println(String.format("Horizontal Position: %d, Depth: %d", horizontalPos, depth));
+	}
+
+	public static void main(String[] args) {
+		final List<String> commands = Arrays.asList("forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2");
+		Day02.move(commands);
+	}
+}
